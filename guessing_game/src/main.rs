1ee908749e6601193284d28bf3d26a5fccd20abd
chengshuo <ext.chengshuo3@jd.com>
2022-03-02 10:33:27 +0800

    完成第二章

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,15 +1,35 @@
 use std::io;
 
+use rand::Rng;
+
 fn main() {
     println!("Guess the number!");
 
-    println!("Please input your guess.");
+    let secret_num = rand::thread_rng().gen_range(1..101);
 
-    let mut guess = String::new();
+    loop {
+        println!("Please input your guess.");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        let mut guess = String::new();
 
-    println!("you guessed: {}", guess);
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("you guessed: {}", guess);
+
+        match guess.cmp(&secret_num) {
+            std::cmp::Ordering::Less => println!("Too small!"),
+            std::cmp::Ordering::Greater => println!("Too big!"),
+            std::cmp::Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
